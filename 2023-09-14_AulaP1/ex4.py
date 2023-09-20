a56fe34926f2fcb829701927dc17f0d663af8d85
Pedro Maia <pedroabmaia@hotmail.com>
2023-09-20 16:24:10 +0100

    Exercice 4

---
+++ 2023-09-14_AulaP1/ex4.py
@@ -0,0 +1,24 @@
+maximum_number = 1000
+
+def isPerfect(value):
+    if value <= 0:
+        return False
+    
+    divisors_sum = 0
+    for i in range(1, value):
+        if value % i == 0:
+            divisors_sum += i
+
+    return divisors_sum == value
+
+
+def main():
+    print("Starting to compute perfect numbers up to " + str(maximum_number))
+
+    for i in range(0, maximum_number):
+        if isPerfect(i):
+            print('Number ' + str(i) + ' is perfect.')
+
+
+if __name__ == "__main__":
+    main()
