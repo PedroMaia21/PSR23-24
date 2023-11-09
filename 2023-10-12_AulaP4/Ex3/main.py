f0d5bce5e20b8d8bd163d903b8270ffd0942a4b2
Pedro Maia <pedroabmaia@hotmail.com>
2023-11-09 10:13:45 +0000

    Started aula 8

--- 2023-10-12_AulaP4/Ex3/main.py
+++ 2023-10-12_AulaP4/Ex3/main.py
@@ -3,17 +3,35 @@
 import cv2
 import numpy as np
 import argparse
+from functools import partial
 
 
-# Global variables
-window_name = 'window - Ex3a'
-image_gray = None
+def onTrackbar(threshold, image_data):
+    black_withe_thr = cv2.getTrackbarPos('Threshold Value', image_data['window_name'])
+    retval, image_thresholded = cv2.threshold(image_data['image'], black_withe_thr, 255, cv2.THRESH_BINARY) #change the image
+    cv2.imshow(image_data['window_name'], image_thresholded) #show the changed image
 
+    min_blue_hue_thr = cv2.getTrackbarPos('Min B/H', image_data['window_name'])
+    min_green_sat_thr = cv2.getTrackbarPos('Min G/S', image_data['window_name'])
+    min_red_val_thr = cv2.getTrackbarPos('Min R/V', image_data['window_name'])
+    max_blue_hue_thr = cv2.getTrackbarPos('Max B/H', image_data['window_name'])
+    max_green_sat_thr = cv2.getTrackbarPos('Max G/S', image_data['window_name'])
+    max_red_val_thr = cv2.getTrackbarPos('Max R/V', image_data['window_name'])
+   
+    _,image_blue_hue = cv2.threshold(image_data['bh'], min_blue_hue_thr, max_blue_hue_thr, cv2.THRESH_BINARY)
+    _,image_green_sat = cv2.threshold(image_data['gs'], min_green_sat_thr, max_green_sat_thr, cv2.THRESH_BINARY)
+    _,image_red_val = cv2.threshold(image_data['rv'], min_red_val_thr, max_red_val_thr, cv2.THRESH_BINARY)
 
-def onTrackbar(threshold):
-    retified_threshold = int(threshold/100*255) #Translates percentage to 0 - 255 value
-    retval, image_thresholded = cv2.threshold(image_gray, retified_threshold, 255, cv2.THRESH_BINARY) #change the image
-    cv2.imshow(window_name, image_thresholded) #show the changed image
+    image_thresholded_2 = cv2.merge((image_blue_hue,image_green_sat,image_red_val))
+
+    cv2.imshow(image_data['window_name']+' Merge', image_thresholded_2)
+    cv2.imshow(image_data['window_name']+' Blue / Hue', image_blue_hue)
+    cv2.imshow(image_data['window_name']+' Green / Saturation', image_green_sat)
+    cv2.imshow(image_data['window_name']+' Red / Value', image_red_val)
+
+def onMouseCallback(event, x, y, *userdata, image_data):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print('x: '+str(x)+'   y:'+str(y))
 
 
 def main():
@@ -22,27 +40,45 @@ def main():
     #------------------------
     
     parser = argparse.ArgumentParser() #Creates an required argument of the image path
-    parser.add_argument('-i', '--image', type=str, required=True,help='Full path to image file.')
+    parser.add_argument('-i', '--image', type=str, required=True,help='Full path to image file.') #path to image argument
+    parser.add_argument('-hs', '--hsv', required=False, help='Use to transform in hsv mode',action='store_true')
     args = vars(parser.parse_args())
 
     image = cv2.imread(args['image'], cv2.IMREAD_COLOR)  # Load an image
+
+    if args['hsv']:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        bh, gs, rv = cv2.split(image)
+    else:
+        bh, gs, rv = cv2.split(image)
+
 
     #------------------------
     # Execution
     #------------------------
 
-    global image_gray # use global var
-    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #Converts to gray scale
-    retval, image_thresholded = cv2.threshold(image_gray, 128, 255, cv2.THRESH_BINARY) #Gets a threshold of the grey image
-    cv2.namedWindow(window_name) #Create the window
+    image_data = {'window_name':'Ex3', 'image': 0, 'hsv':args['hsv'], 'bh':bh, 'gs':gs, 'rv':rv}
+    image_data['image'] = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #Converts to gray scale
+    retval, image_thresholded = cv2.threshold(image_data['image'], 128, 255, cv2.THRESH_BINARY) #Gets a threshold of the grey image
+    cv2.namedWindow(image_data['window_name']) #Create the window
 
-    cv2.createTrackbar('Threshold Value', window_name, 50, 100, onTrackbar)
+    cv2.createTrackbar('Threshold Value', image_data['window_name'], 129, 255, partial(onTrackbar, image_data=image_data))#Creates the first trackbar
+    cv2.setMouseCallback(image_data['window_name'],partial(onMouseCallback, image_data=image_data)) #Executes the mouse callback function
+
+            #MOREEEE TRACKBARS
+    cv2.createTrackbar('Min B/H', image_data['window_name'], 50, 255, partial(onTrackbar, image_data=image_data))#Creates the first trackbar
+    cv2.createTrackbar('Max B/H', image_data['window_name'], 255, 255, partial(onTrackbar, image_data=image_data))#Creates the first trackbar
+    cv2.createTrackbar('Min G/S', image_data['window_name'], 50, 255, partial(onTrackbar, image_data=image_data))#Creates the first trackbar
+    cv2.createTrackbar('Max G/S', image_data['window_name'], 255, 255, partial(onTrackbar, image_data=image_data))#Creates the first trackbar
+    cv2.createTrackbar('Min R/V', image_data['window_name'], 50, 255, partial(onTrackbar, image_data=image_data))#Creates the first trackbar
+    cv2.createTrackbar('Max R/V', image_data['window_name'], 255, 255, partial(onTrackbar, image_data=image_data))#Creates the first trackbar
+    
 
     #------------------------
     # Visualization
     #------------------------
 
-    cv2.imshow(window_name, image_thresholded)  # Display the image
+    cv2.imshow(image_data['window_name'], image_thresholded)  # Display the image
 
     #------------------------
     # Termination
